96ebc29fe7c8752c3a2869605e69a65445408ce6
Jaakko Suutarla <jaakko@suutarla.com>
2017-05-12 08:26:09 +0300

    Upgrade dependencies to latest versios

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.julienxx/clj-slack "0.5.3"]
+                 [org.julienxx/clj-slack "0.5.4"]
                  [org.clojure/data.json "0.2.6"]
-                 [stylefruits/gniazdo "0.4.1"]
-                 [org.clojure/core.async "0.2.374"]])
+                 [stylefruits/gniazdo "1.0.0"]
+                 [org.clojure/core.async "0.3.442"]])
