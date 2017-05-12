1a4b7f69898bb8ad1bff8ddd746f0c972ec1c2f3
Cristian <me@cristian.io>
2017-05-12 09:42:50 -0700

    Updated license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject slack-rtm "0.1.4"
   :description "Slack Real Time Messaging API for Clojure"
   :url "http://github.com/casidiablo/slack-rtm"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "WTFPL"
+            :url "http://www.wtfpl.net/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.julienxx/clj-slack "0.5.4"]
                  [org.clojure/data.json "0.2.6"]
